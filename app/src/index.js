134331d19acc940f6ab864f2051db90ea7212458
Stuart Seguin <stuseguin@gmail.com>
2022-01-11 20:46:00 -0700

    feat: add limit

--- app/src/index.js
+++ app/src/index.js
@@ -7,18 +7,20 @@ const main = async () => {
     const db = client.db("blogHeadlines");
     const collection = db.collection("articles");
 
+    // const source = "https://hnrss.org/frontpage";
+    const source = "https://feeds.feedburner.com/mrmoneymustache/.rss";
+    const limit = 3;
     const parser = new Parser();
-    const feed = await parser.parseURL("https://feeds.feedburner.com/mrmoneymustache/.rss");
-    console.log(feed);
+    // const feed = await parser.parseURL("https://feeds.feedburner.com/mrmoneymustache/.rss");
+    const feed = await parser.parseURL(source);
 
-    for (let {title, link, pubDate} of feed.items) {
+    for (let i = 0; i < feed.items.length && i < limit; i++) {
+        const {title, link, pubDate} = feed.items[i];
         console.log(title, link, pubDate)
-        await collection.updateOne({title}, {$set: {link, pubDate: new Date(pubDate).toISOString(), fetchedDate: new Date().toISOString()}}, {upsert: true});
+        await collection.updateOne({title}, {$set: {source, link, pubDate: new Date(pubDate).toISOString(), fetchedDate: new Date().toISOString()}}, {upsert: true});
     }
 
     await client.close();
-    // const result = await axios.get("https://feeds.feedburner.com/mrmoneymustache");
-    // console.log(result);
 };
 
 main().catch(e => console.error(e));
